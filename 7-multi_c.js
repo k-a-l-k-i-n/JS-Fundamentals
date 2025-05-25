63950775a338e27ea4c00dfb65bea6e55d064d49
Kalkidan <kalkidanasefa7211@gmail.com>
2025-05-25 14:36:06 +0300

    Fix script to print 'C is fun' x times; no output if number <= 0

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,12 +1,13 @@
 const arg = process.argv[2];
 const num = parseInt(arg, 10);
 
-if (isNaN(num) || num <= 0) {
+if (!arg || isNaN(num)) {
   console.log("Missing number of occurrences");
-} else {
+} else if (num > 0) {
   let i = 0;
   while (i < num) {
     console.log("C is fun");
     i++;
   }
 }
+// If num <= 0, print nothing
